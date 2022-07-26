18a942aea4dfacdf715f379fd4863102f0b4fefb
President <aaron.m.wilson@uconn.edu>
2022-07-25 20:52:32 -0400

    created the framework for my employee object tests

---
+++ __tests__/employee.test.js
@@ -0,0 +1,21 @@
+const employee = require("input")
+
+describe("employee" , () => {
+    
+    describe("name" , () => {
+        it("should make a new employee object from input")
+        
+    });
+
+    describe("id" , () => {
+        it("should make a new ID object for the previously created employee")
+
+    });
+
+    describe("email" , () => {
+        it("should make a new email object for the previously created employee")
+
+    });
+});
+
+
